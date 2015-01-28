97729e5591b13ffddec6da5e1e2dd47d3d27af50
Gavin Thomas <gavin_thomas@metaps.com>
2015-01-28 14:38:12 +0900

    Simple calls are working
    Still do do
    * Card Membership calls
    * Code clean up
    * Commenting of usage

--- lib/active_merchant/billing/gateways/econtext.rb
+++ lib/active_merchant/billing/gateways/econtext.rb
@@ -1,105 +1,339 @@
+require 'nokogiri'
+
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
-    class EcontextGateway < Gateway
-      self.test_url = 'https://example.com/test'
-      self.live_url = 'https://example.com/live'
 
-      self.supported_countries = ['US']
-      self.default_currency = 'USD'
+
+    #
+    # == Description
+    # The EcontextGateway class supports interaction with {ECONTEXT's}[http://www.econtext.jp] Payment Gateway Service.
+    # This ActiveMerchant Gateway class supports two forms of interaction with ECONTEXT.
+    #
+    # * Credit Card Payment - Non-membership :- Credit Card must be supplied in Purchase and Authorisation calls
+    # * Credit Card Payment - Member Service :- Credit Card details are stored with ECONTEXT and referenced in future operations with a Card Token
+    #
+    # This gateway class uses the following ActiveMerchant method to ECONTEXT operation mapping
+    # A mapping is defined by a combination of ```fncCode (f)``` and ```paymtCode (p)``` values in the request sent to ECONTEXT
+    #
+    # | ActiveMerchant  |  Non-membership  |  Member Service
+    # | --------------- | ---------------- | --------------------------------
+    # | purchase        |  p=c10/f=10      |  p=c20/f=10
+    # | authorize       |  p=c10/f=8       |  p=c20/f=8
+    # | capture         |  p=c10/f=12      |  p=c20/f=12
+    # | void            |  p=c10/f=20      |  p=c20/f=20
+    # | refund          |  p=c10/f=20      |  p=c20/f=20
+    # | store           |  N/A             |  p=c20/f=01 (Create), p=c20/f=02 (Update)
+    # | unstore         |  N/A             |  p=c20/f=03 (Only deletes card, not the userid)
+    #
+    #
+    class EcontextGateway < Gateway
+
+      PAYMENT_CODE = {
+          cash:                    'A10',
+          card_non_membership:     'C10',
+          card_membership:         'C20'
+      }
+
+      FNC_CODES = {
+        member_serv_register_card:              '01',
+        member_serv_update_card:                '02',
+        member_serv_delete_card:                '03',
+        member_serv_card_refe:                  '04',
+        card_register_and_auth:                 '08', # Need to capture after
+        cash_card_reg_auth_sale:                '10', # Register by cash or card, then auth, then sales
+        capture:                                '12', # Capture card and paypal settlement
+        change_charge_amount:                   '19', # Change the charge amount of order by credit card
+        cancel_order:                           '20',
+        member_serv_register_and_auth:          '21',
+        member_serv_register_auth_capture:      '22',
+        register_card_and_auth:                 '23',
+        register_card_and_auth_capture:         '24',
+        reauth_old_transaction:                 '30'
+      }
+
+      STATUS = {
+          success:                              '1',
+          error_retry_possible:                 '-1',
+          error_retry_impossible_request_fail:  '-2',
+          error_retry_impossible_econ_fail:     '-3',
+          authorization_error:                  '-7'
+      }
+
+      self.test_url = 'https://test.econ.ne.jp/odr/rcv/rcv_odr.aspx'
+      #self.live_url = 'https://example.com/live' - NOT USED
+
+      self.supported_countries = ['JP']
+      self.default_currency = 'JPY'
+      self.money_format = :dollars
       self.supported_cardtypes = [:visa, :master, :american_express, :discover]
 
-      self.homepage_url = 'http://www.example.net/'
-      self.display_name = 'New Gateway'
+      self.homepage_url = 'http://www.econtext.jp' # Japanese Language
+      self.display_name = 'ECONTEXT'
 
       STANDARD_ERROR_CODE_MAPPING = {}
 
+      # Initialize the Gateway
+      #
+      # The gateway requires that valid data defined
+      # in the +options+ hash.
+      #
+      # === Options
+      #
+      #  * <tt>:shop_id => +string+</tt> - This will be assigned to you from ECONTEXT. Known as the SITE CODE (サイトコード)
+      #  * <tt>:chk_code => +string+</tt> - This will be assigned to you from ECONTEXT. Known as the SITE CHECK CODE (サイトチェックコード)
+      #  * <tt>:live_url => +string+</tt> This will be assigned to you from ECONTEXT.
+      #  * <tt>:test => +true+ or +false+</tt> - Force test transactions
+      #
+      # For example:
+      # ```
+      # @gateway = EcontextGateway.new(
+      #     shop_id: 'AA00001',
+      #     chk_code: 'A23SD5',
+      #     live_url: 'https://test.econ.ne.jp/odr/rcv/rcv_odr.aspx' # <--This is the test url
+      # )
+      # ```
+      #
       def initialize(options={})
-        requires!(options, :some_credential, :another_credential)
+        requires!(options, :shop_id, :chk_code, :live_url)
         super
       end
 
       def purchase(money, payment, options={})
-        post = {}
-        add_invoice(post, money, options)
-        add_payment(post, payment)
-        add_address(post, payment, options)
-        add_customer_data(post, options)
 
-        commit('sale', post)
+
+
+        # add_address(post, payment, options)
+        # add_customer_data(post, options)
+
+        if payment.is_a?(ActiveMerchant::Billing::CreditCard)
+          requires!(options, :order_id, :description)
+          # Non-membership
+          post = {
+              'paymtCode' => PAYMENT_CODE[:card_non_membership],
+              'fncCode' => FNC_CODES[:cash_card_reg_auth_sale],
+              'orderID' => options[:order_id], # 6-47 characters, unique per shop_id
+              'sessionID' => options[:order_id] # Same as the order_id
+              # TODO - Need to fix this
+              #'orderDate' => Time.now.getutc.strftime("%Y/%m/%d %H:%M:%S") #yyyy/mm/dd hh:mm:ss
+          }
+          add_invoice(post, money, options)
+          add_econ_payment_page_settings(post, options)
+          add_payment(post, payment)
+        else
+          # Membership
+          requires!(options, :order_id, :description)
+        end
+        commit(post, options)
       end
 
       def authorize(money, payment, options={})
-        post = {}
-        add_invoice(post, money, options)
-        add_payment(post, payment)
-        add_address(post, payment, options)
-        add_customer_data(post, options)
+        # add_address(post, payment, options)
+        # add_customer_data(post, options)
 
-        commit('authonly', post)
+        if payment.is_a?(ActiveMerchant::Billing::CreditCard)
+          requires!(options, :order_id, :description)
+          # Non-membership
+          post = {
+              'paymtCode' => PAYMENT_CODE[:card_non_membership],
+              'fncCode' => FNC_CODES[:card_register_and_auth],
+              'orderID' => options[:order_id], # 6-47 characters, unique per shop_id
+              'sessionID' => options[:order_id], # Same as the order_id
+              # TODO - Need to fix this
+              #'orderDate' => Time.now.getutc.strftime("%Y/%m/%d %H:%M:%S") #yyyy/mm/dd hh:mm:ss
+          }
+          add_invoice(post, money, options)
+          add_econ_payment_page_settings(post, options)
+          add_payment(post, payment)
+        else
+          # Membership
+          requires!(options, :order_id, :description)
+        end
+        commit(post, options)
       end
 
       def capture(money, authorization, options={})
-        commit('capture', post)
+        # add_invoice(post, money, options)
+        # add_payment(post, payment)
+        # add_address(post, payment, options)
+        # add_customer_data(post, options)
+
+        if true # TODO
+          requires!(options, :order_id)
+          # Non-membership
+          post = {
+              'paymtCode' => PAYMENT_CODE[:card_non_membership],
+              'fncCode' => FNC_CODES[:capture],
+              'orderID' => options[:order_id], # 6-47 characters, unique per shop_id
+              'ordAmount' => localized_amount(money, 'JPY').to_i.to_s, # 1-999999 single-byte characters
+              'shipDate' => Time.now.getutc.strftime("%Y/%m/%d") # This is the capture date
+              # TODO - Need to fix this
+              #'orderDate' => Time.now.getutc.strftime("%Y/%m//%d %H:%M:%S") #yyyy/mm/dd hh:mm:ss
+          }
+        else
+          # Membership
+          requires!(options, :order_id)
+        end
+        commit(post, options)
       end
 
       def refund(money, authorization, options={})
-        commit('refund', post)
+        if money.nil?
+          void(authorization, options)
+        else
+          # TODO
+          # We need to do a 'Change' call on the API
+        end
+
       end
 
       def void(authorization, options={})
-        commit('void', post)
+
+        if true # TODO
+          requires!(options, :order_id)
+          # Non-membership
+          post = {
+              'paymtCode' => PAYMENT_CODE[:card_non_membership],
+              'fncCode' => FNC_CODES[:cancel_order],
+              'orderID' => options[:order_id] # 6-47 characters, unique per shop_id
+          }
+        else
+          # Membership
+          requires!(options, :order_id)
+        end
+        commit(post, options)
+
+      end
+
+      def store(payment, options = {})
+        if payment.is_a?(ActiveMerchant::Billing::CreditCard)
+          requires!(options, :customer)
+          post = {
+              'paymtCode' => PAYMENT_CODE[:card_membership],
+              'fncCode' => FNC_CODES[:member_serv_register_card],
+              'cduserID' => options[:customer],
+              'retokURL' => 'http://www.example.com', # TODO Dummy, not going to be used?
+              'retngURL' => 'http://www.example.com', # TODO Dummy, not going to be used?
+              'ordAmount' => localized_amount(100, 'JPY').to_i.to_s, # TODO - Dummy value here OK
+              'ordAmountTax' => '0',
+          }
+          add_econ_payment_page_settings(post, options)
+          add_payment(post, payment)
+        else
+          raise ArgumentError, 'payment must be a Credit card (ActiveMerchant::Billing::CreditCard)'
+        end
+        commit(post)
       end
 
       def verify(credit_card, options={})
-        MultiResponse.run(:use_first_response) do |r|
-          r.process { authorize(100, credit_card, options) }
-          r.process(:ignore_result) { void(r.authorization, options) }
-        end
+        raise NotImplementedError, 'verify operation is not supported'
       end
 
       private
 
       def add_customer_data(post, options)
+        # map the token onto cduserID for token versions
       end
 
       def add_address(post, creditcard, options)
       end
 
       def add_invoice(post, money, options)
-        post[:amount] = amount(money)
-        post[:currency] = (options[:currency] || currency(money))
+        post['itemName'] = options[:description] # single-byte 22 characters
+        post['ordAmount'] = localized_amount(money, 'JPY').to_i.to_s # 1-999999 single-byte characters
+        post['ordAmountTax'] = '0'
+        post['commission'] = '0'
+      end
+
+      def add_econ_payment_page_settings(post, options)
+        post['ecnEntry'] = 0 # Not utilize ECON payment page
+        post['Language'] = 1 # 0 Japanese 1 English # TODO, make configurable?
       end
 
       def add_payment(post, payment)
+        post['econCardno'] = payment.number
+        post['cardExpdate'] = "#{payment.year.to_s}#{'%02d' % payment.month}" # 'yyyymm' format
+        post['payCnt'] = '00' # Default to 'lump-sum payment'
+        post['cd3secFlg'] = 0 # Do NOT activate 3d secure
+        post['CVV2'] = '%04d' % payment.verification_value
       end
 
       def parse(body)
-        {}
+
+        response = {}
+
+        doc = Nokogiri::XML(CGI.unescapeHTML(body), nil, 'Shift_JIS')
+        body = doc.xpath('//result')
+        body.children.each do |node|
+          if node.text?
+            next
+          elsif (node.elements.size == 0)
+            response[node.name.downcase.to_sym] = node.text.encode('utf-8')
+          else
+            node.elements.each do |childnode|
+              name = "#{node.name.downcase}_#{childnode.name.downcase}"
+              response[name.to_sym] = childnode.text.encode('utf-8')
+            end
+          end
+        end
+        response
       end
 
-      def commit(action, parameters)
-        url = (test? ? test_url : live_url)
-        response = parse(ssl_post(url, post_data(action, parameters)))
+      def commit(parameters, options)
+        url = (test? ? test_url : @options[:live_url])
+
+        headers = {
+            'Content-Type' => 'application/x-www-form-urlencoded;charset=shift_jis'
+        }
+
+        # response = parse(ssl_post(url, post_data(parameters), headers))
+
+        uri = URI.parse(url)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+        http.ssl_version = :SSLv3
+
+        puts post_data(parameters)
+
+        response = parse(http.post(uri.request_uri, post_data(parameters), headers).body)
 
         Response.new(
           success_from(response),
           message_from(response),
           response,
-          authorization: authorization_from(response),
+          authorization: authorization_from(response, options),
           test: test?
         )
       end
 
       def success_from(response)
+        result_code_from(response) == STATUS[:success]
+      end
+
+      def result_code_from(response)
+        response[:status].encode('UTF-8') unless response[:status].nil?
       end
 
       def message_from(response)
+        response[:info].encode('UTF-8') unless response[:status].nil?
       end
 
-      def authorization_from(response)
+      def authorization_from(response, options)
+        {
+            ecn_token: response[:ecnToken],
+            user_token: response[:cduserID],
+            order_id: options[:order_id]
+        }
       end
 
-      def post_data(action, parameters = {})
+      def post_data(parameters = {})
+        post = {
+            'shopID' => @options[:shop_id],
+            'chkCode' => @options[:chk_code],
+        }
+        request = post.merge(parameters).map {|key,value| "#{key}=#{CGI.escape(value.to_s.encode('Shift_JIS'))}"}.join("&")
+        request
       end
     end
   end
--- test/remote/gateways/remote_econtext_test.rb
+++ test/remote/gateways/remote_econtext_test.rb
@@ -1,109 +1,196 @@
 require 'test_helper'
 
+# == Description
+# ECONTEXT supports both Credit Card based operations, and those where a token is provided
+# which represents Credit Card details previously stored with ECONTEXT
+#
+# This test class will test the options were CREDIT CARD details are supplied.
+#
 class RemoteEcontextTest < Test::Unit::TestCase
   def setup
     @gateway = EcontextGateway.new(fixtures(:econtext))
 
-    @amount = 100
-    @credit_card = credit_card('4000100011112224')
-    @declined_card = credit_card('4000300011112220')
+    @amount = 10000
+    # TODO Why does this fail, this should be a CVV2 checked card
+    # @credit_card = credit_card('4123450131003312',
+    #                            {:brand => 'visa',
+    #                             :verification_value => '815',
+    #                             :month => 3,
+    #                             :year => (Time.now.year + 1),
+    #                            })
+
+    @credit_card = credit_card('4980111111111111',
+                               {:brand => 'visa',
+                                :verification_value => '815',
+                                :month => 3,
+                                :year => (Time.now.year + 1),
+                               })
+
+    @declined_card_c1430 = credit_card('4980111111111112',
+                                       {:brand => 'visa',
+                                        :verification_value => '815',
+                                        :month => 3,
+                                        :year => (Time.now.year + 1),
+                                       })
+    @declined_card_c1483 = credit_card('4980111111111113',
+                                       {:brand => 'visa',
+                                        :verification_value => '815',
+                                        :month => 3,
+                                        :year => (Time.now.year + 1),
+                                       })
+
+    # TODO
+    @billing_address = {
+      company:  'Widgets Inc',
+      city:     'Tokyo',
+      state:    '13', # TOKYO - http://www.unece.org/fileadmin/DAM/cefact/locode/Subdivision/jpSub.htm
+      zip:      '163-6038',
+      country:  'JP'
+    }
 
     @options = {
-      order_id: '1',
-      billing_address: address,
-      description: 'Store Purchase'
+        order_id: Time.now.getutc.strftime("%Y%m%d%H%M%S%L"),
+        description: 'dummy'
     }
   end
 
   def test_successful_purchase
+    stamp = Time.now.getutc.strftime("%Y%m%d%H%M%S%L")
+    @options = {
+        order_id: stamp,
+        description: "#{stamp}Sale"
+    }
     response = @gateway.purchase(@amount, @credit_card, @options)
     assert_success response
-    assert_equal 'REPLACE WITH SUCCESS MESSAGE', response.message
+    assert_equal '正常', response.message
   end
 
-  def test_failed_purchase
-    response = @gateway.purchase(@amount, @declined_card, @options)
+  def test_failed_purchase_c1430
+    response = @gateway.purchase(@amount, @declined_card_c1430, @options)
     assert_failure response
-    assert_equal 'REPLACE WITH FAILED PURCHASE MESSAGE', response.message
+    assert_equal 'C1430', response.params['infocode']
+    assert_equal '-7', response.params['status']
+    assert_equal 'カード与信失敗(02-00)', response.message
+  end
+
+  def test_failed_purchase_c1483
+    response = @gateway.purchase(@amount, @declined_card_c1483, @options)
+    assert_failure response
+    assert_equal 'C1483', response.params['infocode']
+    assert_equal '-7', response.params['status']
+    assert_equal 'カード与信失敗(01-04)', response.message
   end
 
   def test_successful_authorize_and_capture
+    stamp = Time.now.getutc.strftime("%Y%m%d%H%M%S%L")
+    @options = {
+        order_id: stamp,
+        description: "#{stamp}Auth"
+    }
     auth = @gateway.authorize(@amount, @credit_card, @options)
     assert_success auth
+    assert_equal '正常', auth.message
 
-    assert capture = @gateway.capture(nil, auth.authorization)
+    @options = {
+        order_id: stamp
+    }
+    assert capture = @gateway.capture(@amount, auth.authorization, @options)
     assert_success capture
   end
 
-  def test_failed_authorize
-    response = @gateway.authorize(@amount, @declined_card, @options)
+  def test_failed_authorize_c1430
+    stamp = Time.now.getutc.strftime("%Y%m%d%H%M%S%L")
+    @options = {
+        order_id: stamp,
+        description: "#{stamp}Auth"
+    }
+    response = @gateway.authorize(@amount, @declined_card_c1430, @options)
     assert_failure response
+    assert_equal 'C1430', response.params['infocode']
+    assert_equal '-7', response.params['status']
+    assert_equal 'カード与信失敗(02-00)', response.message
   end
 
-  def test_partial_capture
-    auth = @gateway.authorize(@amount, @credit_card, @options)
-    assert_success auth
-
-    assert capture = @gateway.capture(@amount-1, auth.authorization)
-    assert_success capture
-  end
-
+  # def test_partial_capture
+  #   auth = @gateway.authorize(@amount, @credit_card, @options)
+  #   assert_success auth
+  #
+  #   assert capture = @gateway.capture(@amount-1, auth.authorization)
+  #   assert_success capture
+  # end
+  #
   def test_failed_capture
-    response = @gateway.capture(nil, '')
+    stamp = Time.now.getutc.strftime("%Y%m%d%H%M%S%L")
+    @options = {
+        order_id: stamp,
+    }
+    response = @gateway.capture(@amount, nil, @options)
     assert_failure response
   end
 
   def test_successful_refund
+    stamp = Time.now.getutc.strftime("%Y%m%d%H%M%S%L")
+    @options = {
+        order_id: stamp,
+        description: "#{stamp}Sale"
+    }
     purchase = @gateway.purchase(@amount, @credit_card, @options)
     assert_success purchase
 
-    assert refund = @gateway.refund(nil, purchase.authorization)
+    @options = {
+        order_id: stamp
+    }
+    assert refund = @gateway.refund(nil, purchase.authorization, @options)
     assert_success refund
   end
 
-  def test_partial_refund
-    purchase = @gateway.purchase(@amount, @credit_card, @options)
-    assert_success purchase
-
-    assert refund = @gateway.refund(@amount-1, purchase.authorization)
-    assert_success refund
-  end
-
-  def test_failed_refund
-    response = @gateway.refund(nil, '')
-    assert_failure response
-  end
-
+  # def test_partial_refund
+  #   purchase = @gateway.purchase(@amount, @credit_card, @options)
+  #   assert_success purchase
+  #
+  #   assert refund = @gateway.refund(@amount-1, purchase.authorization)
+  #   assert_success refund
+  # end
+  #
+  # def test_failed_refund
+  #   response = @gateway.refund(nil, '')
+  #   assert_failure response
+  # end
+  #
   def test_successful_void
+    stamp = Time.now.getutc.strftime("%Y%m%d%H%M%S%L")
+    @options = {
+        order_id: stamp,
+        description: "#{stamp}Auth"
+    }
     auth = @gateway.authorize(@amount, @credit_card, @options)
     assert_success auth
 
-    assert void = @gateway.void(auth.authorization)
+    @options = {
+        order_id: stamp
+    }
+    assert void = @gateway.void(auth.authorization, @options)
     assert_success void
   end
 
   def test_failed_void
-    response = @gateway.void('')
+    stamp = Time.now.getutc.strftime("%Y%m%d%H%M%S%L")
+    @options = {
+        order_id: stamp
+    }
+    response = @gateway.void('', @options)
     assert_failure response
   end
 
   def test_successful_verify
-    response = @gateway.verify(@credit_card, @options)
-    assert_success response
-    assert_match %r{REPLACE WITH SUCCESS MESSAGE}, response.message
-  end
-
-  def test_failed_verify
-    response = @gateway.verify(@declined_card, @options)
-    assert_failure response
-    assert_match %r{REPLACE WITH FAILED PURCHASE MESSAGE}, response.message
-    assert_equal Gateway::STANDARD_ERROR_CODE[:card_declined], response.error_code
+    assert_raise(NotImplementedError){ @gateway.verify(@credit_card, @options) }
   end
 
   def test_invalid_login
     gateway = EcontextGateway.new(
-      login: '',
-      password: ''
+      shop_id: 'fake',
+      chk_code: 'fake',
+      live_url: 'http://www.example.com'
     )
     response = gateway.purchase(@amount, @credit_card, @options)
     assert_failure response
